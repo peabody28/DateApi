42920ae003f04aa0e75cf628876adf370aeb3bde
Harbacheuski Maksim <gorbachewski.m@gmail.com>
2023-05-07 22:19:23 +0300

    minimal api (date)

---
+++ DateApi/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/date", () =>
+{
+    return DateTime.UtcNow;
+})
+.WithName("Date");
+
+app.Run();
