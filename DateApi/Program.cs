2f200dda33120fb8775d97c659a65f78b80deba0
Harbacheuski Maksim <gorbachewski.m@gmail.com>
2023-05-11 20:59:35 +0300

    cors service adding

--- DateApi/Program.cs
+++ DateApi/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 app.UseSwagger();
